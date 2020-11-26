52a6aada7ad7460de97b5f5f0c9fbcbab6b9f0d6
Matt <mattprindle042@gmail.com>
2020-11-26 10:30:31 -0500

    added birthday Property to Customer and display it in details

---
+++ UdemyVideoSite/Migrations/202011261523530_customerBirthdayProperty.cs
@@ -0,0 +1,18 @@
+namespace UdemyVideoSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class customerBirthdayProperty : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Birthdate", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Birthdate");
+        }
+    }
+}
