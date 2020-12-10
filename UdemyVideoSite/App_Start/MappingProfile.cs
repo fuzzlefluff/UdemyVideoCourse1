71c5c0e1d4f127f43c08bd933356c02be734158c
Matt <mattprindle042@gmail.com>
2020-12-10 13:31:28 -0500

    api uses Dto and automapper to map between Customer and CustomerDto. Also updated the api to return in camel case.

---
+++ UdemyVideoSite/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using UdemyVideoSite.Dtos;
+using UdemyVideoSite.Models;
+
+namespace UdemyVideoSite.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
