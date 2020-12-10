67452c0219e4dc609db771ecd324856d4a82b945
Matt <mattprindle042@gmail.com>
2020-12-10 13:56:08 -0500

    api extended to handle CRUD for movies

---
+++ UdemyVideoSite/Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using UdemyVideoSite.Models;
+
+namespace UdemyVideoSite.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public Genre Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+        [Required]
+        public DateTime? DateAdded { get; set; }
+        [Required]
+        [Range(1,20)]
+        public byte Stock { get; set; }
+    }
+}
